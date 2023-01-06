5fc236fbee88d7354dac780be45ad9674120aaec
Jason Hu <jasonhu515@gmail.com>
2023-01-06 15:30:30 +0100

    add compiled jupyter book

---
+++ _build/jupyter_execute/added-contents.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Trying different contents (.md)
+# 
+# This page contains some random components.
+# 
+# (section-label-1)=
+# ## First section
+
+# In[1]:
+
+
+# print 100 empty lines
+for i in range(100):
+    print('placeholder')
+
+
+# Here is a [reference back to the intro](intro.md). Here's a ref to [](section-label-1).
